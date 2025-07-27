fd9e34b5b85832846e6a422b3c368f07601805ea
Python <priyanshichandak2706@gmail.com>
2025-07-27 16:14:31 +0530

    Basics

---
+++ Loops/Validate input.py
@@ -0,0 +1,7 @@
+while True:
+    number=int(input("Enter a number between 1-10:"))
+    if 1<=number<=10:
+        print("Good choice")
+        break
+    else:
+        print("Invalid number, Try again.")
